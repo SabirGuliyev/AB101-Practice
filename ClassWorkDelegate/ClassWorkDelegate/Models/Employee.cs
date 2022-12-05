671819d6a30115fb4544e43252de850f63e22b84
SabirQ <sabirmg@code.edu.az>
2022-12-05 20:31:51 +0400

    12/5 ClassWork added

---
+++ ClassWorkDelegate/ClassWorkDelegate/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWorkDelegate.Models
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public string Gmail { get; set; }
+        public Employee(string name, string gmail)
+        {
+            Name = name;
+            Gmail = gmail;
+        }
+    }
+}
---
+++ ClassWorkDelegate/ClassWorkDelegate/Program.cs
@@ -0,0 +1,37 @@
+﻿using ClassWorkDelegate.Models;
+using System.Text;
+
+namespace ClassWorkDelegate
+{
+    delegate void RegisterEmployee( Employee employee);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("sabir", "sabir.guliyev");
+            RegisterEmployee register = new RegisterEmployee(Capitalize);
+            register += CreateGmail;
+
+            Console.WriteLine($"Before changes: name={employee.Name} gmail={employee.Gmail}");
+
+            register.Invoke( employee);
+
+            Console.WriteLine($"After changes: name={employee.Name} gmail={employee.Gmail}");
+        }
+        public static void Capitalize( Employee employee)
+        {
+            StringBuilder str=new StringBuilder();
+            str.Append(Char.ToUpper(employee.Name[0]));
+
+            for (int i = 1; i < employee.Name.Length; i++)
+            {
+                str.Append(Char.ToLower(employee.Name[i]));
+            }
+            employee.Name = str.ToString();
+        }
+        public static void CreateGmail( Employee employee)
+        {
+            employee.Gmail += "@gmail.com";
+        }
+    }
+}
