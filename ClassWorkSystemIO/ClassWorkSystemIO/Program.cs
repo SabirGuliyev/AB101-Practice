168cdd379a91ca5a7ae23b6e357acb157685ff23
SabirQ <sabirmg@code.edu.az>
2022-12-08 15:21:18 +0400

    12/8 classWork added

---
+++ ClassWorkSystemIO/ClassWorkSystemIO/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassWorkSystemIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = Path.Combine(Directory.GetCurrentDirectory(), @"../../../", "AB101");
+
+            Directory.CreateDirectory(path);
+            File.Create(Path.Combine(path,"Student.cs"));
+        }
+    }
+}
